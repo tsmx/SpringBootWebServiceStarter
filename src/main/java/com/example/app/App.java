9659e82091ea04bcb52c3784809c48f1996e5cdf
tsmx <strs@freenet.de>
2021-01-22 21:28:58 +0100

    initial commit

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -1,17 +1,12 @@
 package com.example.app;
 
-import com.example.dto.SimpleMessageDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication(scanBasePackages = "com.example.services")
+@SpringBootApplication
 public class App {
 
-    public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
-    }
+  public static void main(String... args) {
+    SpringApplication.run(App.class, args);
+  }
 }
