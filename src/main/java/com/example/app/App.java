295885eaae40f97bb868a6a7f927c60327234b6a
tsmx <strs@freenet.de>
2021-01-23 22:25:36 +0100

    Added JSON response and simple test.

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -1,12 +1,24 @@
 package com.example.app;
 
+import com.example.utils.SimpleMessage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class App {
 
-  public static void main(String... args) {
-    SpringApplication.run(App.class, args);
-  }
+    @RequestMapping("/")
+    public ResponseEntity<SimpleMessage> home() {
+        return new ResponseEntity<>(new SimpleMessage("Hello Spring Boot World!"),
+                HttpStatus.OK);
+    }
+
+    public static void main(String... args) {
+        SpringApplication.run(App.class, args);
+    }
 }
