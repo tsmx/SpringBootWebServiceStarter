3ab0b905c311b7c7a6ffa4a933417cc0f4ca07a1
tsmx <Kalinke123$>
2021-01-25 20:34:18 +0100

    Refactoring.

--- src/main/java/com/example/app/HomePathService.java
+++ src/main/java/com/example/app/HomePathService.java
@@ -1,17 +1,18 @@
 package com.example.app;
 
 import com.example.dto.SimpleMessageDto;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-public class App {
+@RestController
+public class HomePathService {
 
-    public static void main(String... args) {
-        SpringApplication.run(App.class, args);
+    @RequestMapping("/")
+    public ResponseEntity<SimpleMessageDto> home() {
+        return new ResponseEntity<>(new SimpleMessageDto("Hello Spring Boot World!"),
+                HttpStatus.OK);
     }
+
 }
