275f48d8c07c94be47246f53f259155d004dd54d
tsmx <Kalinke123$>
2022-11-07 23:13:50 +0100

    Imports optimized.

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -1,12 +1,7 @@
 package com.example.app;
 
-import com.example.dto.SimpleMessageDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(scanBasePackages = "com.example.services")
 public class App {
